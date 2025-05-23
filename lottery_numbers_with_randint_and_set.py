84ba8ec23ba113a6ba1fddc78dfbee92060cb5db
Toni Mertanen <t.mertanen@hotmail.com>
2025-05-23 22:31:49 +0300

    Refactor generate_lottery_numbers function to accept parameters for number count and range

--- lottery_numbers_with_randint_and_set.py
+++ lottery_numbers_with_randint_and_set.py
@@ -5,26 +5,28 @@ def remove_duplicates(numbers):
     return list(set(numbers))
 
 
-def generate_lottery_numbers():
+def generate_lottery_numbers(how_many_numbers: int, min_number: int, max_number: int) -> list:
     """
     Generate a list of 7 unique random lottery numbers between 1 and 49.
     """
     from random import randint
 
     numbers = []
-    while len(numbers) < 7:
-        number = randint(1, 40)
+    while len(numbers) < how_many_numbers:
+        number = randint(min_number, max_number)
         numbers.append(number)
         numbers = remove_duplicates(numbers)
-    return numbers
+    return sorted(numbers)
 
 
 def main():
     """
     Main function to generate and print lottery numbers.
     """
-    lottery_numbers = generate_lottery_numbers()
-    print("Your lottery numbers are:", ", ".join(str(num) for num in lottery_numbers))
+    lottery_numbers = generate_lottery_numbers(7, 1, 40)
+    # print("Your lottery numbers are:", ", ".join(str(num) for num in lottery_numbers))
+    for number in lottery_numbers:
+        print(number)
 
 
 if __name__ == "__main__":
