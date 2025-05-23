6ae1fea75c0dd612b4c27e42920815a6a1f595a1
Toni Mertanen <t.mertanen@hotmail.com>
2025-05-23 23:25:43 +0300

    Add functions to simulate rolling two dice and play a dice game

--- dice_simulation.py
+++ dice_simulation.py
@@ -3,6 +3,39 @@ from random import choice
 noppa = {"A": [3, 3, 3, 3, 3, 6],
          "B": [2, 2, 2, 5, 5, 5],
          "C": [1, 4, 4, 4, 4, 4]}
+    
+
+def roll_two_dices(dice1: str, dice2: str) -> tuple:
+    """
+    Simulate rolling two types of dice a specified number of times.
+    :param dice1: The first type of dice to roll.
+    :param dice2: The second type of dice to roll.
+    :return: A tuple containing the results of the rolls.
+    """
+    result1 = choice(noppa[dice1])
+    result2 = choice(noppa[dice2])
+    results = result1, result2
+    return results
+
+
+def play_dice_game(dice1: str, dice2: str, times: int) -> tuple[int, int, int]:
+    """
+    Count the results of rolling two types of dice.
+    :return: A tuple containing the counts of each result 
+        (wins for dice1, wins for dice2, ties).
+    """
+    wins1 = 0
+    wins2 = 0
+    ties = 0
+    for _ in range(times):
+        result = roll_two_dices(dice1, dice2)
+        if result[0] > result[1]:
+            wins1 += 1
+        elif result[0] < result[1]:
+            wins2 += 1
+        else:
+            ties += 1
+    return wins1, wins2, ties
 
 
 def roll_dice(dice: str) -> int:
@@ -12,6 +45,7 @@ def roll_dice(dice: str) -> int:
     :return: The result of the dice roll.
     """
     return choice(noppa[dice])
+
 
 def main():
     """
@@ -25,6 +59,11 @@ def main():
     print()
     for i in range(20):
         print(roll_dice("C"), " ", end="")
+    print()
+    tulos = play_dice_game("A", "C", 1000)
+    print(tulos)
+    tulos = play_dice_game("B", "B", 1000)
+    print(tulos)
 
 
 if __name__ == "__main__":
